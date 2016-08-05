ac907bc769969092fde9e1405214e06989e95df0
Sam <samgitrepos@gmail.com>
2016-08-05 10:44:11 +0100

    file update

--- server/server.js
+++ server/server.js
@@ -5,16 +5,17 @@ var middleware = require('../middleware');
 
 
 
+
 app.get('/', middleware.userAuth, function (req, res) {
     res.send('index home');
 });
 
+
 app.get('/about', middleware.detectUser, function (req, res) {
     res.send('Hello about');
-
 });
 
-app.use(express.static(__dirname + '/public'));
 
+app.use(express.static(__dirname + '/public'));
 app.listen(setPort);
 console.log('server running');
