6db186050625e61492d59ed37018e2e2fa42989c
Sam <samgitrepos@gmail.com>
2016-08-05 09:27:50 +0100

    Initialize ropo

---
+++ server/server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var setPort = 5000;
+
+
+var today = new Date().toString();
+
+var middleware = {
+
+    userAuth : function (req, res,  next) {
+
+        console.log('User has been authenticated ' + today);
+        next();
+    },
+    detectUser: function (req, res, next) {
+        console.log('User was detected on  ' + today);
+        next();
+    }
+};
+
+
+
+app.get('/', middleware.userAuth, function (req, res) {
+    res.send('index home');
+});
+
+app.get('/about', middleware.detectUser, function (req, res) {
+    res.send('Hello about');
+
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(setPort);
+console.log('server running');
